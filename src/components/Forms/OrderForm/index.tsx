1c5d50a63106dcb6c925d6120454c5175fc7178c
Larissa Ferreira <larissa.ferreira@kobe.io>
2022-04-18 15:39:12 -0300

    app-decode-finalizado

---
+++ src/components/Forms/OrderForm/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import firestore from "@react-native-firebase/firestore"
+import { Alert } from 'react-native';
+import { Form, Title } from './styles';
+import { Input } from '@components/Controllers/Input';
+import { Button } from '@components/Controllers/Button';
+import { TextArea } from '@components/Controllers/TextArea';
+
+export function OrderForm() {
+  const [patrimony, setPatrimony] = useState('');
+  const [description, setDescription] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleNewOrder() {
+    setIsLoading(true);
+
+    firestore()
+    .collection('orders')
+    .add({
+      patrimony,
+      description,
+      status: 'open',
+      created_at: firestore.FieldValue.serverTimestamp()
+    })
+    .then(() => Alert.alert("Chamado", "Chamado aberto com sucesso!"))
+    .finally(() => setIsLoading(false));
+  }
+
+  return (
+    <Form>
+      <Title>Novo chamado</Title>
+      <Input placeholder="Número do Patrimônio" onChangeText={setPatrimony} />
+      <TextArea placeholder="Descrição" onChangeText={setDescription} />
+
+      <Button title="Enviar chamado" isLoading={isLoading} onPress={handleNewOrder} />
+    </Form>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import auth, {FirebaseAuthTypes} from "@react-native-firebase/auth"
+import { Home } from '@screens/Home';
+import { AuthRoutes } from './auth.routes';
+
+export function Routes() {
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null >(null)
+
+
+  useEffect(() => {
+    const subscribe = auth(). onAuthStateChanged(setUser)
+
+    return subscribe
+
+
+  },[])
+
+  return (
+    <NavigationContainer>
+      {user ? <Home /> : <AuthRoutes />}
+    </NavigationContainer>
+  );
+}
